08228f5ce5247c545ab3ab16acaef11eb68aba3c
Dmitry Malikov <color404051h@gmail.com>
2012-05-05 10:33:44 +0400

    Scrobbler: add scrobbleDB

--- Loh/Scrobbler.hs
+++ Loh/Scrobbler.hs
@@ -1,16 +1,17 @@
 module Loh.Scrobbler
-  ( nowPlaying, scrobbleTrack
+  ( nowPlaying, scrobbleDB, scrobbleTrack
   ) where
 
 import Control.Applicative ((<$>))
 import Control.Arrow (left)
-import Control.Monad (void)
+import Control.Monad (forM_, void)
 import Data.Time (formatTime, getCurrentTime)
 import System.Locale (defaultTimeLocale)
 
 import qualified Network.Lastfm as LFM
 import qualified Network.Lastfm.API.Track as Track
 
+import Loh.DB (getDB)
 import Loh.Types
 
 nowPlaying ∷ (LFM.APIKey, LFM.SessionKey, LFM.Secret) → TrackInfo → IO ()
@@ -31,6 +32,24 @@ scrobbleTrack (ak, sk, s) ti = do
     nts ← read . formatTime defaultTimeLocale "%s" <$> getCurrentTime
     void $ left (error . show) <$>
         Track.scrobble (LFM.Timestamp nts
+                       , Just $ LFM.Album $ album ti
+                       , LFM.Artist $ artist ti
+                       , LFM.Track $ track ti
+                       , Nothing
+                       , Nothing
+                       , Nothing
+                       , Nothing
+                       , Nothing
+                       , Nothing
+                       , Nothing)
+                       ak sk s
+
+scrobbleDB ∷ (LFM.APIKey, LFM.SessionKey, LFM.Secret) → IO ()
+scrobbleDB (ak, sk, s) = do
+  db ← getDB
+  forM_ db $ \(DBRecord (Timestamp ts) ti) →
+    void $ left (error . show) <$>
+        Track.scrobble (LFM.Timestamp ts
                        , Just $ LFM.Album $ album ti
                        , LFM.Artist $ artist ti
                        , LFM.Track $ track ti
