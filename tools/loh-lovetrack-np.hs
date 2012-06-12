ca3b9273f0685c672d84abc883bfba8be11e0561
Dmitry Malikov <color404051h@gmail.com>
2012-06-12 16:19:26 +0400

    LoveTrack: add some messages, unicodify

--- tools/loh-lovetrack-np.hs
+++ tools/loh-lovetrack-np.hs
@@ -1,11 +1,19 @@
-#!/usr/bin/env runhaskell
+{-# LANGUAGE UnicodeSyntax #-}
+module Main where
+
+import Control.Monad (forM_)
 
 import Loh.Config
 import Loh.LoveTrack
 import Loh.Player
+import Loh.Types
 
-main :: IO ()
+main ∷ IO ()
 main = do
-  config <- getConfig
-  mapM_ (loveTrack config) =<< getCurrentTracks
-
+  config ← getConfig
+  currentTracks ← getCurrentTracks
+  case length currentTracks of
+    0 → putStrLn "Error: Nothing to love"
+    _ → forM_ currentTracks $ \t → do
+      loveTrack config t
+      putStrLn $ "Loved " ++ (artist t) ++ " - " ++ (track t)
