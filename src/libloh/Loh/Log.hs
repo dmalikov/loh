6fe3152d056652c52036a217ac427e5a1187132b
Dmitry Malikov <color404051h@gmail.com>
2012-06-12 17:17:51 +0400

    Log: improved readability of log

--- src/libloh/Loh/Log.hs
+++ src/libloh/Loh/Log.hs
@@ -12,14 +12,14 @@ import Text.Printf
 import Loh.Types
 
 getTime ∷ IO String
-getTime = formatTime defaultTimeLocale "%T" <$> getCurrentTime
+getTime = formatTime defaultTimeLocale "%F [%T]" <$> getCurrentTime
 
 logNowPlaying ∷ TrackInfo → IO ()
 logNowPlaying ti = do
   time ← getTime
-  printf "%s: Now Playing: %s - %s\n" time (artist ti) (track ti)
+  printf "%s: now playing \"%s - %s\"\n" time (artist ti) (track ti)
 
 logScrobble ∷ TrackInfo → IO ()
 logScrobble ti = do
   time ← getTime
-  printf "%s: Scrobble: %s - %s\n" time (artist ti) (track ti)
+  printf "%s: scrobbling \"%s - %s\"\n" time (artist ti) (track ti)
