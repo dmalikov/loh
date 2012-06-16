361db8d8c4eef37db11a0dd0cdfdc3f2c407e243
Dmitry Malikov <color404051h@gmail.com>
2012-06-16 13:33:45 +0400

    Eventer: correct track storing

--- src/libloh/Loh/Eventer.hs
+++ src/libloh/Loh/Eventer.hs
@@ -6,13 +6,14 @@ import Data.Traversable (sequenceA)
 
 import qualified Data.Map as M
 
+import Loh.DB
 import Loh.Log
 import Loh.Player
 import Loh.Scrobbler
 import Loh.Types
 
 fetchDelay ∷ Int
-fetchDelay = 10 {- delay between players info fetching, sec -}
+fetchDelay = 20 {- delay between players info fetching, secs -}
 
 fetchAccur ∷ Int
 fetchAccur = 3
@@ -21,28 +22,41 @@ scrobbleDuration ∷ Duration
 scrobbleDuration = 0.51
 
 isConsistent ∷ TrackInfo → TrackInfo → Bool
-isConsistent α β = α == β && abs (((-) `on` currentSec) α β) < fetchDelay + fetchAccur
+isConsistent α β =
+  α == β && abs (((-) `on` currentSec) α β) < fetchDelay + fetchAccur
 
 isReadyToScrobble ∷ Maybe Duration → TrackInfo → Bool
 isReadyToScrobble Nothing _ = False
 isReadyToScrobble (Just startDuration) ti =
   duration ti - startDuration > scrobbleDuration
 
-manageTrackInfo ∷ LFMConfig → TrackInfo → Maybe (TrackInfo, Maybe Duration) → IO (TrackInfo, Maybe Duration)
+manageTrackInfo ∷ LFMConfig
+                → TrackInfo
+                → Maybe (TrackInfo, Maybe Duration)
+                → IO (TrackInfo, Maybe Duration)
 manageTrackInfo config new Nothing = do
-    nowPlaying config new
+    _ ← nowPlaying config new
     return (new, Just $ duration new)
 manageTrackInfo config new (Just (old, d)) =
   if isConsistent old new
     then do
-      nowPlaying config new
-      logNowPlaying new
+      -- logMessage "Trying update now playing"
+      stnp ← nowPlaying config new
+      -- logMessage "Finished updating now playing"
+      case stnp of
+        ScrobbleDone → logNowPlaying new
+        ScrobbleFailed → logNowPlayingFailed new
       if isReadyToScrobble d new
         then do
+          -- logMessage "Trying scroblle"
           st ← scrobbleTrack config new
+          -- logMessage "Finished scrobble"
           case st of
             ScrobbleDone → logScrobble new
-            ScrobbleFailed → putStrLn "Scrobble Failed"
+            ScrobbleFailed → do
+              logScrobbleFailed new
+              logDBStore new
+              store new
           return (new, Nothing)
         else
           return (new, d)
@@ -52,8 +66,12 @@ manageTrackInfo config new (Just (old, d)) =
 intersect ∷ Ord k ⇒ (α → Maybe β → γ) → M.Map k α → M.Map k β → M.Map k γ
 intersect f ma mb = M.mapWithKey (\k a → f a (M.lookup k mb)) ma
 
-updateCurrentTracks ∷ LFMConfig → PlayersInfo → PlayersInfoToScrobble → IO PlayersInfoToScrobble
-updateCurrentTracks c newP oldP = sequenceA $ intersect (manageTrackInfo c) newP oldP
+updateCurrentTracks ∷ LFMConfig
+                    → PlayersInfo
+                    → PlayersInfoToScrobble
+                    → IO PlayersInfoToScrobble
+updateCurrentTracks c newP oldP =
+  sequenceA $ intersect (manageTrackInfo c) newP oldP
 
 eventer ∷ LFMConfig → IO α
 eventer c = eventer' c M.empty
