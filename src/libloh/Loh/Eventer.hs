1229882a3e48d8a83454fccba0bd8551b992d52d
Dmitry Malikov <color404051h@gmail.com>
2012-06-30 22:34:47 +0400

    Eventer: add track consistence checking before scrobble

--- src/libloh/Loh/Eventer.hs
+++ src/libloh/Loh/Eventer.hs
@@ -5,22 +5,35 @@ import Data.Function (on)
 
 import Loh.DB
 import Loh.Log
-import Loh.InfoMocp
 import Loh.InfoMpd
 import Loh.Scrobbler
 import Loh.Types
 
-toSecs ∷ Int → Int
-toSecs = (* 1000000)
-
 threadDelayS ∷ Int → IO ()
 threadDelayS = threadDelay . toSecs
+  where toSecs = (* 1000000)
 
 fetchDelay ∷ Int
-fetchDelay = 20 {- delay between players info fetching, secs -}
+fetchDelay = 10 {- delay between players info fetching, secs -}
 
 fetchAccur ∷ Int
 fetchAccur = 3
+
+-- | Fetch player for specific trackInfo until it will be able to scrobble
+followUntilReadyToScrobble ∷ IO (Maybe TrackInfo) → TrackInfo → Int → IO Bool
+followUntilReadyToScrobble getPlayerInfo ti timeToFollow
+  | timeToFollow < 0 = return True
+  | otherwise = do
+    logMessage $ "following... " ++ show timeToFollow ++ " to complete"
+    threadDelayS fetchDelay
+    trackInfo' ← getPlayerInfo
+    case trackInfo' of
+      Nothing → return False
+      Just newti → do
+        let diff = (subtract `on` currentSec) ti newti ∷ Int
+        if newti == ti && abs (diff - fetchDelay) < fetchAccur
+          then followUntilReadyToScrobble getPlayerInfo newti $ timeToFollow - fetchDelay
+          else return False
 
 servePlayer ∷ LFMConfig → IO (Maybe TrackInfo) → Maybe TrackInfo → IO α
 servePlayer c getPlayerInfo maybeOldTrack = do
@@ -40,19 +53,19 @@ servePlayer c getPlayerInfo maybeOldTrack = do
         else do
           let delayToScrobble = round . (* 0.51) . toRational $ totalSec new
           logMessage $ "waiting " ++ show delayToScrobble ++ " toScrobble"
-          threadDelayS delayToScrobble
-          trackInfo ← getPlayerInfo
-          case trackInfo of
-            Just freshNew | new == freshNew && (subtract `on` currentSec) freshNew new < delayToScrobble + fetchAccur → do
-              st ← scrobbleTrack c freshNew
+          isSameTrack ← followUntilReadyToScrobble getPlayerInfo new delayToScrobble
+          if isSameTrack
+            then do
+              st ← scrobbleTrack c new
               case st of
-                ScrobbleDone → logScrobble freshNew
+                ScrobbleDone → logScrobble new
                 ScrobbleFailed → do
-                  logScrobbleFailed freshNew
-                  logDBStore freshNew
-                  store freshNew
+                  logScrobbleFailed new
+                  logDBStore new
+                  store new
               servePlayer c getPlayerInfo Nothing
-            _ → servePlayer c getPlayerInfo trackInfo
+            else
+              servePlayer c getPlayerInfo Nothing
     _ → servePlayer c getPlayerInfo maybeNewTrack
 
 eventer ∷ LFMConfig → IO ()
