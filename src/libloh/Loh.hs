9b9701dee74eaa28ce94353294effeb95bd5e991
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-07-27 23:47:27 +0400

    Merge log-get-session tool into base executable.

--- src/libloh/Loh.hs
+++ src/libloh/Loh.hs
@@ -1,7 +1,48 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import Loh.Config (readConfig)
+import Control.Applicative ((<$>))
+import Control.Exception (SomeException, catch)
+import Prelude hiding (catch)
+
+import Network.Lastfm.JSON.Auth
+import Network.Lastfm (APIKey(..), Secret(..))
+
+import Loh.Config (LConfig(..), readConfig, writeConfig)
 import Loh.Eventer
 
+
 main ∷ IO ()
-main = eventer =<< readConfig
+main =
+  do decodedConfig ← catch readConfig $ \(_ ∷ SomeException) → genConfigSkeleton
+     case decodedConfig of
+       Just config → eventer config
+       Nothing → error "Malformed ~/.lohrc"
+
+
+genConfigSkeleton ∷ IO (Maybe LConfig)
+genConfigSkeleton =
+  do token ← extract <$> getToken apiKey
+     putStrLn $ unlines
+       [ "~/.lohrc seems to be missing"
+       , "You need correct session key to use loh properly."
+       , "Authorize your token: " ++ getAuthorizeTokenLink apiKey token
+       , "And then press any key."
+       ]
+     getChar
+     sk ← extract <$> getSession apiKey token secret
+     writeConfig LConfig
+       { players = []
+       , lfmConfig = (apiKey, sk, secret)
+       }
+     putStrLn $ unlines
+       [ "Session key is successfuly written to ~/.lohrc"
+       , "Add players you wish to scrobble from and restart loh."
+       ]
+     return Nothing
+ where
+  apiKey = APIKey "34a538d1ce307a257c695bcc7e031392"
+  secret = Secret "4bd6283d6441cdf842d03ba8ab7a6ddf"
+
+  extract (Left e) = error $ show e
+  extract (Right v) = v
